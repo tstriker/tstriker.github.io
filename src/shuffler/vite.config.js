9b1c6d547d9e12c7d156e43568b19c5fc275fbbd
Tom Striker <toms.baugis@gmail.com>
2023-10-27 14:57:15 +0100

    wip

--- src/shuffler/vite.config.js
+++ src/shuffler/vite.config.js
@@ -6,9 +6,13 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    base: "/shuffler",
     resolve: {
         alias: {
             "@": fileURLToPath(new URL(".", import.meta.url)),
         },
+    },
+    build: {
+        outDir: "../../shuffler",
     },
 });
